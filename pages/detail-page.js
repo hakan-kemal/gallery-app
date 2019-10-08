1389815c89051f7a4a9ed2e47d1b8facd71132c5
hakan0814 <h.k.cuhadar@gmail.com>
2019-10-08 15:43:17 +0200

    Set up js files and declare submitComment

---
+++ pages/detail-page.js
@@ -0,0 +1,11 @@
+/* select submit button for a javascript 
+event and assign a function to it */
+
+/* A user can leave their name and 
+write a comment, when they click submit, 
+their name and comment will be displayed 
+on the page. */
+
+function submitComment() {
+    console.log('Hello!')
+}
